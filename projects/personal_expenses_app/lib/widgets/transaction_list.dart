5eb77fa147f2b07cb88db7676df7a4eac366e4b9
naoldenko <naoldenko@gmail.com>
2022-12-20 11:21:00 -0500

    update

--- projects/personal_expenses_app/lib/widgets/transaction_list.dart
+++ projects/personal_expenses_app/lib/widgets/transaction_list.dart
@@ -3,10 +3,15 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+
 @override
   Widget build(BuildContext context) {
     return Column(
-        children: _userTransactions.map((tx) {
+        children: transactions.map((tx) {
           return Card(
             child: Row(
               children: <Widget>[
